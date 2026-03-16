ee895faf60d54a2573b77f39b65b55b3fd14d1a0
Ethan Wen <75800128+wen-ethan@users.noreply.github.com>
2026-03-16 19:13:25 -0400

    fix scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,30 @@ import ApPhysicsC from './pages/projects/ApPhysicsC'
 import GoogleFormAutofill from './pages/projects/GoogleFormAutofill'
 import BlogTemplate from './pages/projects/BlogTemplate'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="/projects/spatial-computing" element={<SpatialComputing />} />
-      <Route path="/projects/ap-physics-c" element={<ApPhysicsC />} />
-      <Route path="/projects/google-form-autofill" element={<GoogleFormAutofill />} />
-      <Route path="/projects/blog-template" element={<BlogTemplate />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/projects/spatial-computing" element={<SpatialComputing />} />
+        <Route path="/projects/ap-physics-c" element={<ApPhysicsC />} />
+        <Route path="/projects/google-form-autofill" element={<GoogleFormAutofill />} />
+        <Route path="/projects/blog-template" element={<BlogTemplate />} />
+      </Routes>
+    </>
   )
 }
